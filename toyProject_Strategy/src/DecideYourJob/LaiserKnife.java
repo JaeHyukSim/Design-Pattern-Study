345f08b0db345557cd5a8af17091707b0fd030c5
jaehyuksim <jaehyuksim@gmail.com>
2019-12-13 14:36:19 +0900

    toyProject Strategy

---
+++ toyProject_Strategy/src/DecideYourJob/LaiserKnife.java
@@ -0,0 +1,10 @@
+package DecideYourJob;
+
+public class LaiserKnife implements Weapon{
+	public void weapon() {
+		System.out.println("최신무기 laiser를 들었다");
+	}
+	public String getId() {
+		return "LaiserKnife";
+	}
+}
